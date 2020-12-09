4bbea39aee73835daab4aedea824c9058d1760fa
subta90 <the.bird.nobility298@gmail.com>
2020-12-09 22:19:44 +0900

    とりあえず固定値で計算する

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,13 @@ module Main where
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do
+    putStrLn $ show $ minimumUpTimePerDay 18 $ remainingTime 0.0 140.0
+
+-- 最低稼働時間を満たすための残り稼働時間を算出
+remainingTime :: Double -> Double -> Double
+remainingTime currentTime minimumUpTime = minimumUpTime - currentTime
+
+-- 1日あたりの最低稼働時間を算出
+minimumUpTimePerDay :: Int -> Double -> Double
+minimumUpTimePerDay remainingDay remainingTime = remainingTime / (realToFrac (remainingDay :: Int))
